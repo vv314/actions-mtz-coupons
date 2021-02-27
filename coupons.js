0f14ea5308d80201d10ecd4d994f4acdd4ef42c6
Vincent Lee <vv314@foxmail.com>
2021-02-28 02:09:35 +0800

    添加领取步骤

--- coupons.js
+++ coupons.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 const ECODE = {
-  API: 1,
-  AURH: 2,
+  AURH: 1,
+  API: 2,
   NETWOEK: 3,
   RUNTIME: 4
 }
@@ -11,13 +11,17 @@ const actUrl =
 let $request
 
 function genRequest(token) {
+  const host = 'https://activityunion-marketing.meituan.com'
   const cookie = `token=${token}`
   let result
 
-  return async (api) => {
+  return async (api, data = {}) => {
+    const url = api.indexOf('http') == 0 ? api : host + api
+
     try {
-      result = await fetch(api, {
+      result = await fetch(url, {
         method: 'POST',
+        data: data,
         headers: {
           cookie: cookie
         }
@@ -31,22 +35,42 @@ function genRequest(token) {
 }
 
 async function getRule() {
-  const api = 'https://activityunion-marketing.meituan.com/mtz/index'
-
-  return $request(api).then((res) => res.data.rule)
+  return $request('/mtz/index').then((res) => res.data.rule)
 }
 
-async function queryGrabResult() {
-  const api = 'https://activityunion-marketing.meituan.com/mtz/queryGrabResult'
-  const res = await $request(api)
+async function grabCoupon() {
+  const res = await $request('/mtz/grab')
 
   if (res.code == 0) return res.data
-
   if (res.code == 3) throw { code: ECODE.AURH, msg: res.msg }
 
   throw { code: ECODE.API }
 }
 
+async function queryGrabResult() {
+  const res = await $request('/mtz/queryGrabResult')
+
+  if (res.code == 0) return res.data
+  if (res.code == 3) throw { code: ECODE.AURH, msg: res.msg }
+
+  throw { code: ECODE.API }
+}
+
+function formatCoupons(coupons) {
+  return coupons.map((item) => ({
+    name: item.couponName,
+    etime: item.etime,
+    amount: item.couponAmount,
+    amountLimit: item.amountLimit,
+    useCondition: item.useCondition
+  }))
+}
+
+/**
+ * 领取优惠券
+ * @param  {String} token 用户 token
+ * @return {Promise(<Object>)} 结果
+ */
 async function getCoupons(token) {
   if (!token) {
     return {
@@ -58,18 +82,21 @@ async function getCoupons(token) {
   $request = genRequest(token)
 
   try {
-    const grabResult = await queryGrabResult()
+    await grabCoupon()
+
     const rule = await getRule()
+    const grabResult = await queryGrabResult()
 
     return {
       code: 0,
       data: {
         rule: rule,
         sendStatus: grabResult.sendStatus,
-        coupons: grabResult.coupons,
+        coupons: formatCoupons(grabResult.coupons),
         useUrl: grabResult.defaultUseUrl,
         phone: grabResult.phoneNum
-      }
+      },
+      msg: '成功'
     }
   } catch (e) {
     console.log('getCoupons error', e)
